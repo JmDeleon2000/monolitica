7e040c4162374a672da7b426298bcf1c71f128bf
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-12-15 22:42:28 -0600

    Remover soporte para modelos de tensorflow
    Al parecer tienen conflicto as librerías

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageOps
 import plotly.express as px
 import pandas as pd
 import os
+from time import sleep
 
 
 st.title('Monolítico')
@@ -21,6 +22,33 @@ def shownegative(img):
                         mime='text/png',
                     )
 
+def histAndCsv(uploaded_file, img):
+    listdir = os.listdir()
+    for i, e in enumerate(np.array(img)):
+        csvname = f'{uploaded_file.name}_{hex(i)}.csv'
+        if not csvname in listdir:
+            df = pd.DataFrame()
+            df['Red'] =     [j[0] for j in e]
+            df['Green'] =   [j[1] for j in e]
+            df['Blue'] =    [j[2] for j in e]
+            df.to_csv(csvname)
+        else:
+            df = pd.read_csv(csvname)
+        if do_hists:
+            fig = px.histogram(df, opacity=0.75, nbins=20)
+            st.plotly_chart(fig)
+        if do_csvs:
+            with open(csvname, 'rb') as csv:
+                st.download_button(
+                    label=f'Descargar {csvname}',
+                    data=csv,
+                    file_name=csvname,
+                    mime='text/csv',
+                )
+
+
+
+
 
 uploaded_file = st.file_uploader("Elija un archivo", type=['.png', '.jpg'])
 
@@ -34,6 +62,9 @@ if uploaded_file is not None:
         do_hists = st.checkbox('Histogramas')
     with col2:
         do_csvs = st.checkbox('Quiero descargar un .csv')
+        
+    
+    mandelbrot = st.checkbox('Superponer el set de mandelbrot')
 
     procesar = st.button('Procesar')
 
@@ -41,31 +72,26 @@ if uploaded_file is not None:
         if negative:
             shownegative(img)
 
-        listdir = os.listdir()
         if do_hists or do_csvs:
+            histAndCsv(uploaded_file, img)
+    
 
-            for i, e in enumerate(np.array(img)):
-                csvname = f'{uploaded_file.name}_{hex(i)}.csv'
-                if not csvname in listdir:
-                    df = pd.DataFrame()
-                    df['Red'] =     [j[0] for j in e]
-                    df['Green'] =   [j[1] for j in e]
-                    df['Blue'] =    [j[2] for j in e]
-                    df.to_csv(csvname)
-                else:
-                    df = pd.read_csv(csvname)
-
-                if do_hists:
-                    fig = px.histogram(df, opacity=0.75, nbins=20)
-                    st.plotly_chart(fig)
-
-                if do_csvs:
-                    with open(csvname, 'rb') as csv:
-                        st.download_button(
-                            label=f'Descargar {csvname}',
-                            data=csv,
-                            file_name=csvname,
-                            mime='text/csv',
+        
+    if mandelbrot:
+        mandel = Image.effect_mandelbrot(size=img.size, extent=(-2, -2, 2, 2), quality=100)
+        alpha = st.slider('Alpha', 0.0, 1.0, 0.5, 0.01)
+        blendImage = Image.blend(img, mandel.convert(img.mode), alpha)
+        blendImage.save(f'{uploaded_file.name}_mandelblend.png')
+        st.image(blendImage, width=500)
+        with open(f'{uploaded_file.name}_mandelblend.png', 'rb') as file:
+            st.download_button(
+                            label=f'Descargar',
+                            data=file,
+                            file_name=f'{uploaded_file.name}_mandelblend.png',
+                            mime='text/png',
                         )
+
+
+
 
 
